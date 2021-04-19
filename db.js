ff61fd77883190f176ce54c6dc4521247c4cc0a8
victor novais <victor.noovais@gmail.com>
2021-04-18 23:44:00 -0300

    processamento e exibições das transaçoes

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-
+//Conexão com o db
 async function connect() {
 
     if(global.connection && global.connection.state !== 'disconnected'){
@@ -15,6 +15,31 @@ async function connect() {
     return connection
 }
 
-connect()
+//Processamento de transações com o db
 
-module.exports = {}
+async function transaction(transaction){
+    const conn = await connect()
+    const sql = 'INSERT INTO transaction(value,payment_type,card_number,card_owner,validaty,CVV,description) VALUES (?,?,?,?,?,?,?);'
+    const values = [
+        transaction.value,
+        transaction.payment_type,
+        transaction.card_number,
+        transaction.card_owner,
+        transaction.validaty,
+        transaction.cvv,
+        transaction.description]
+    
+    await conn.query(sql,values)
+}
+
+//Consulta das transações 
+async function queryTransaction(){
+    const conn = await connect()
+    const sql = 'SELECT * FROM transaction;'
+    const [rows] = await conn.query(sql)
+
+    return rows
+
+}
+
+module.exports = {transaction,queryTransaction}
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const db = require('./db.js')
+async function app() {
+    const db = require('./db.js')
+    let tran = {
+        value:'2000',
+        payment_type:'debit_card',
+        card_number:'1234123412344321',
+        card_owner:'Ana Carolina Brites da Silva',
+        validaty:'022022',
+        cvv:'123',
+        description:'Piano'
+    }
+
+    var transactions_register = await db.queryTransaction()
+
+    db.transaction(tran)
+}
+
+app()
