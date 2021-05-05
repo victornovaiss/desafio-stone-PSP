7d759f3c05917b2eddfeb31f6272ef02b15bc8a4
victor novais <victor.noovais@gmail.com>
2021-05-04 23:53:13 -0300

    modificações finais

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 //Conexão com o db
 async function connect() {
 
-    if(global.connection && global.connection.state !== 'disconnected'){
+    if (global.connection && global.connection.state !== 'disconnected') {
         return global.connection
     }
 
     const mysql = require('mysql2/promise')
-    const connection = await mysql.createConnection('mysql://root:pipoca123@localhost:3306/provider')
-   
+    const connection = await mysql.createConnection('mysql://usuario:senha@localhost:porta-de-hopedagem/nome-da-tabela')
+
     console.log('Conectou!')
 
     global.connection = connection
@@ -17,7 +17,7 @@ async function connect() {
 
 //Processamento de transações com o db
 
-async function transaction(transaction){
+async function transaction(transaction) {
     const conn = await connect()
     const sql = 'INSERT INTO transaction(value,payment_type,card_number,card_owner,validaty,CVV,description) VALUES (?,?,?,?,?,?,?);'
     const values = [
@@ -28,12 +28,15 @@ async function transaction(transaction){
         transaction.validaty,
         transaction.cvv,
         transaction.description]
-    
-    await conn.query(sql,values)
+
+    await conn.query(sql, values)
+
+
+
 }
 
 //Consulta das transações 
-async function queryTransaction(){
+async function queryTransaction() {
     const conn = await connect()
     const sql = 'SELECT * FROM transaction;'
     const [rows] = await conn.query(sql)
@@ -42,4 +45,43 @@ async function queryTransaction(){
 
 }
 
-module.exports = {transaction,queryTransaction}
+//Função dos recebíveis que é executado na função transaction 
+
+async function insertPayable(payable) {
+    
+    const conn = await connect()
+    const sql = 'INSERT INTO payable(transaction_cod,status,payment_date,fee) VALUES (?,?,?,?)'
+    
+    const value = [
+        payable.transaction_cod,
+        payable.status,
+        payable.date,
+        payable.fee
+    ]
+    
+    await conn.query(sql,value)
+}
+
+//Atualizar a conta
+async function updateCash(){
+    const conn = await connect()
+    
+    let transactions = await queryTransaction()
+
+    let availableToPayment = 0
+    let waitingFunds = 0
+
+    transactions.forEach(element => {
+        if (element.status == 'paid'){
+            availableToPayment += element.fee 
+        }else{
+            waitingFunds += element.fee
+        }
+    })
+
+    const sql = 'UPDATE provider.cash SET paid = ?, waiting_funds = ? WHERE (account_id = 1);' 
+
+    await conn.query(sql,[availableToPayment,waitingFunds])
+}
+
+module.exports = { transaction, queryTransaction, insertPayable, updateCash}
--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 async function app() {
     const db = require('./db.js')
+
+    let date = new Date()
+    let datePlus30 = new Date()
+    
+    datePlus30.setDate(date.getDate()+30)
+    
+    let dateISO = (date.toISOString()).slice(0, 10)
+    let dateISOplus30 = (datePlus30.toISOString()).slice(0, 10)
+    let card_number = '4321432143210009'
+    
     let tran = {
-        value:'2000',
-        payment_type:'debit_card',
-        card_number:'1234123412344321',
-        card_owner:'Ana Carolina Brites da Silva',
-        validaty:'022022',
-        cvv:'123',
-        description:'Piano'
+        value: '230',
+        payment_type: 'credit_card',
+        card_number:`******${card_number.slice(6,10)}*****`,
+        card_owner: 'Vicente Brites Novais',
+        validaty: '1220',
+        cvv: '436',
+        description: 'Bolachinha'
+    }
+    
+    await db.transaction(tran)
+    
+    var transactions_register = await db.queryTransaction()
+    
+    var lastTransaction = transactions_register[transactions_register.length - 1]
+    
+    let payable = {}
+
+    if (lastTransaction.payment_type == 'debit_card') {
+        payable = {
+            transaction_cod: lastTransaction.cod,
+            status: 'paid',  
+            date: dateISO,
+            fee: Number(lastTransaction.value) * 0.97
+        }
+    } else {
+        payable = {
+            transaction_cod: lastTransaction.cod,
+            status:'waiting_funds',
+            date: dateISOplus30,
+            fee: Number(lastTransaction.value) * 0.95
+        }
     }
 
-    var transactions_register = await db.queryTransaction()
+    await db.insertPayable(payable)
+    
 
-    db.transaction(tran)
+    console.log(transactions_register)
 }
 
 app()
